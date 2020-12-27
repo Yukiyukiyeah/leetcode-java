67d1594281554d1b34a0fd86384454b8f25a3c1a
Yuki <yukitang@mail.bnu.edu.cn>
2020-12-27 19:31:36 +0800

    12.27

--- J35.java
+++ J35.java
@@ -1,4 +1,12 @@
 /*
+ * @Author: your name
+ * @Date: 2020-12-25 21:01:44
+ * @LastEditTime: 2020-12-27 19:31:09
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /leetcode-java/J35.java
+ */
+/*
 // Definition for a Node.
 class Node {
     int val;
@@ -12,19 +20,16 @@ class Node {
     }
 }
 */
-class Solution {
-  Map<Node, Node> visited = new HashMap<>();
-  public Node copyRandomList(Node head) {
-      return copy(head);
 
-  }
-  private Node copy(Node head) {
-      if (head == null) return null;
-      if (visited.containsKey(head)) return visited.get(head);
-      Node newNode = new Node(head.val);
-      visited.put(head, newNode);
-      newNode.next = copyRandomList(head.next);
-      newNode.random = copyRandomList(head.random);
-      return newNode;
-  }
+class Solution {
+    Map<Node, Node> visited = new HashMap<>();
+    public Node copyRandomList(Node head) {
+        if (head == null) return null;
+        if (visited.containsKey(head)) return visited.get(head);
+        Node newNode = new Node(head.val);
+        visited.put(head, newNode);
+        newNode.next = copyRandomList(head.next);
+        newNode.random = copyRandomList(head.random);
+        return newNode;
+    }
 }
